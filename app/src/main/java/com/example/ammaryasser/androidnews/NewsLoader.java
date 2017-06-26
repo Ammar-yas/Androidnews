7d186d887809a17bbef42aea8b1adac4107a781b
Ammar Yasser <plus.ammaryasser@gmail.com>
2017-06-26 19:52:44 +0200

    Adding the NewsLoader class

---
+++ app/src/main/java/com/example/ammaryasser/androidnews/NewsLoader.java
@@ -0,0 +1,28 @@
+package com.example.ammaryasser.androidnews;
+
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader {
+
+    String url;
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+        super.onStartLoading();
+    }
+
+    public NewsLoader(Context context, String url) {
+        super(context);
+        this.url = url;
+    }
+
+    @Override
+    public List<news> loadInBackground() {
+        return QueryUtils.fetchNewsData(url);
+    }
+}
